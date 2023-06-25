6d30de855ceed36ddce4c56ce0d0685e07cc1478
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-06-25 18:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	let name="";
+	let age="";
+	Constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	function getter(){
+		return name;
+	}
+	function setter(){
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+		function study(){
+			return (this.name, is studying);
+		}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach(){
+			return (this.name, is teaching);
+		}
+}
 
 // Do not change the code below this line
 window.Person = Person;
